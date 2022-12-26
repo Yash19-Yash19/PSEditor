17ff75acc28333475e4fba3b15898290942edd4b
Yash <73273674+Yash19-Yash19@users.noreply.github.com>
2022-12-27 02:14:46 +0530

    Update pencilsketch_webapp.py

--- pencilsketch_webapp.py
+++ pencilsketch_webapp.py
@@ -67,6 +67,6 @@ else:
     st.write("You haven't uploaded any image file")
 
 
-st.write("Courtesy: itsyou  - [Sketch Code](https://github.com/Yash19-Yash19/PSEditor)")
+st.write("Courtesy: itsyou  - [Sketch Code](https://github.com/Yash19-Yash19)")
 
-st.markdown("![](https://github.com/Yash19-Yash19)")
+st.markdown("![](https://avatars.githubusercontent.com/u/73273674?s=400&u=2eedf54ee0e7d1bfd8eafc4a78f574074ace7130&v=4)")
