4c31b2220af8a85506596b3184e78563f786105e
Yash Liladhar Amarapurkar <yashamr19@gmail.com>
2023-05-18 13:33:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@
 import streamlit as st #web app and camera
 import numpy as np # for image processing 
 from PIL import Image #Image processing 
-import cv2 #computer vision 
+from cv2 import cv2 #computer vision 
 import os
 import time
 
